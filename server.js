d08d320c56b46e26d278857aa6386ccc38266f15
ab1123 <asarma626@gmail.com>
2025-01-14 21:35:50 +0530

    fix:updating render.yaml

--- server.js
+++ server.js
@@ -25,9 +25,11 @@ app.use('/api/blogs', blogRoutes);
 app.use(errorHandler);
 
 // Connect to MongoDB
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('MongoDB connection error:', err));
