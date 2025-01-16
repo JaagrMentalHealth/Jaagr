cfdf5431a71e9f3dd6e99615440b926e6176a695
ab1123 <asarma626@gmail.com>
2025-01-17 00:14:34 +0530

    fix:updated middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -25,7 +25,7 @@ exports.protect = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded);
-    const currentUser = await User.findOne({ userName: decoded.id.id });
+    const currentUser = await User.findOne({ userName: decoded.id});
 
     if (!currentUser) {
       return res
